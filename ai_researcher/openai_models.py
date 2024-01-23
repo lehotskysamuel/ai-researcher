5b0793d0cc08c3400f65f94b7622dddd4fa0cfef
Samuel Lehotsky <lehotsky@atlas.sk>
2024-01-23 15:40:44 +0100

    Added organization to OpenAI calls

--- ai_researcher/openai_models.py
+++ ai_researcher/openai_models.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 from langchain_community.chat_models import ChatOpenAI
 
 
-class OpenAIModels(Enum):
+class OpenAiModels(Enum):
     GPT_4_TURBO = "gpt-4-1106-preview"
     GPT_4_VISION = "gpt-4-vision-preview"
     GPT_3_5_TURBO = "gpt-3.5-turbo-1106"
@@ -13,17 +13,22 @@ class OpenAIModels(Enum):
 
 load_dotenv()
 openai_api_key = os.getenv("OPENAI_KEY")
+openai_org_id = os.getenv("OPENAI_ORG_ID")
 
 gpt4 = ChatOpenAI(
-    api_key=openai_api_key,
-    model_name=OpenAIModels.GPT_4_TURBO.value,
+    openai_api_key=openai_api_key,
+    openai_organization=openai_org_id,
+    model_name=OpenAiModels.GPT_4_TURBO.value,
 )
 
 gpt4_vision = ChatOpenAI(
     api_key=openai_api_key,
-    model_name=OpenAIModels.GPT_4_VISION.value,
+    openai_organization=openai_org_id,
+    model_name=OpenAiModels.GPT_4_VISION.value,
 )
 
 gpt3 = ChatOpenAI(
-    api_key=openai_api_key, model_name=OpenAIModels.GPT_3_5_TURBO.value
+    api_key=openai_api_key,
+    openai_organization=openai_org_id,
+    model_name=OpenAiModels.GPT_3_5_TURBO.value,
 )
